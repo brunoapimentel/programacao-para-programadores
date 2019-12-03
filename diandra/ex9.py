f57681ab27f4f1feb3555533c147d0cf3f965689
diandra <di.andrade90@gmail.com>
2019-12-02 21:54:46 -0300

    Add exercise 9 made for barueri class

---
+++ diandra/ex9.py
@@ -0,0 +1,50 @@
+#Descobrir se o aluno passou ou não na faculdade
+
+alunos = [
+    {
+        'nome': 'Ana Beatriz',
+        'notas': [7, 6.5, 9, 4.5]
+    },
+    {
+        'nome': 'Eric',
+        'notas': [6.5, 6, 5, 7]
+    },
+    {
+        'nome': 'Camila',
+        'notas': [7, 8, 9, 10]
+    },
+    {
+        'nome': 'Lucas',
+        'notas': [8, 5.5, 6, 7]
+    },
+    {
+        'nome': 'Nicolas',
+        'notas': [9, 8, 10, 10]
+    },
+    {
+        'nome': 'Natalia',
+        'notas': [5.5, 4.5, 6, 4]
+    },
+    {
+        'nome': 'Renan',
+        'notas': [4, 7, 5.5, 6.5]
+    }
+]
+
+valor = (alunos[0]['notas'])
+print(valor)
+
+media = 6
+
+for aluno in alunos:
+    aluno_media = 0 
+    for nota in aluno['notas']:
+        aluno_media += nota
+
+    
+    aluno_media = aluno_media / int(nota)
+    
+    if aluno_media >= media:
+        print(f'o aluno {aluno["nome"]} foi aprovado com a média de {aluno_media}')
+    elif aluno_media < media:
+        print(f'o aluno {aluno["nome"]} não passou com a média de {aluno_media}')
